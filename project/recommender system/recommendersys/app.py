6ecbceb85fbd33829bdfcbc3003c9a51d70468cf
sarav003 <saravana300896@gmail.com>
2023-01-24 15:29:05 +0530

    second commit

--- project/recommender system/recommendersys/app.py
+++ project/recommender system/recommendersys/app.py
@@ -1,10 +1,14 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import pandas as pd
 top50=pickle.load(open(r'C:\Users\sarav\Downloads\top50.pkl','rb'))
 pt=pickle.load(open(r'C:\Users\sarav\Downloads\pivot.pkl','rb'))
 books=pickle.load(open(r'C:\Users\sarav\Downloads\books.pkl','rb'))
 cosine_sim=pickle.load(open(r'C:\Users\sarav\Downloads\cosine_sim.pkl','rb'))
+amaz=pickle.load(open(r'C:\Users\sarav\Downloads\amaz_data.pkl','rb'))
+cont_cos_sim=pickle.load(open(r'C:\Users\sarav\Downloads\cont_cos_sim.pkl','rb'))
+df=pickle.load(open(r'C:\Users\sarav\Downloads\final.pkl','rb'))
 
 app = Flask(__name__)
 
@@ -15,12 +19,12 @@ def index():
                             image=list(top50['Image-URL-M'].values),
                             ratings=list(top50['Book-Rating'].values)) 
 
-@app.route('/recommend')
-def recommend_ui():
-    return render_template('recommend.html')
+@app.route('/colab')
+def recommend_rate():
+    return render_template('colab.html')
 
 @app.route('/recommend_books',methods=['post'])
-def recommend():
+def recommend_boo_rate():
     user_input = request.form.get('user_input')
     ind=np.where(pt.index==user_input)[0][0]
     items=sorted(list(enumerate(cosine_sim[ind])),key=lambda u:u[1],reverse=True)[1:11]
@@ -35,7 +39,30 @@ def recommend():
         data.append(item)
     print(data)
          
-    return render_template('recommend.html',data=data)
+    return render_template('colab.html',data=data)
+
+@app.route('/content')
+def recommend_cont():
+    return render_template('content.html')
+
+@app.route('/recommend_content',methods=['post'])
+def recommend_boo_cont():
+    content_input=request.form.get('content_input')
+    indices = pd.Series(amaz['Title'])
+    idx = indices[indices == content_input].index[0]
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+    top_5_indices = list(score_series.iloc[1:6].index)
+    da=[]
+    for i in top_5_indices:
+      recommended_movies=[]
+      temp_df=(df[df['Title']==df.index[i]])
+      recommended_movies.append(list(df['Title'])[i])
+      recommended_movies.append(list(df['authors'])[i])
+      recommended_movies.append(list(df['image'])[i])
+      recommended_movies.append(list(df['review/score'])[i])
+      da.append(recommended_movies)
+    
+    return render_template('content.html',dta=da)
 
 if __name__ == '__main__':
     app.run(debug=True)
